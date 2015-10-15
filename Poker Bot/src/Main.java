46d05832bcc058d6876f61c65f4c5158d03f4f97
rasic <rasic_m@live.com>
2015-10-15 23:07:09 +0200

    Jase pored StojSase

--- Poker Bot/src/Main.java
+++ Poker Bot/src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("RADI PRVI PUT");
 		System.out.println("RADI DRUGI PUT");
-		System.out.println("RASICU PEDERU! ");
+		System.out.println("MARKOVICU PEDERU! ");
 		// Miric ponovo jase!
+		// Jase pored StojSase!
 	}
 
 }
