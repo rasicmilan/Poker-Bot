00c981f3f9a820cc237edaaa8a125d0b857979c5
stefanmrkvc <stefanmrkvc@gmail.com>
2015-10-15 22:38:49 +0200

    Jebem li ga dal ovo radi bemu dete plekano

--- Poker Bot/src/Main.java
+++ Poker Bot/src/Main.java
@@ -1,9 +1,9 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("RADI PRVI PUT");
 		System.out.println("RADI DRUGI PUT");
+		System.out.println("RASICU PEDERU! ");
 	}
 
 }
